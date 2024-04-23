8e61a4e5440579d8a996e5d1a81a7f6e90677c89
stx <80523829+stx3plus1@users.noreply.github.com>
2024-04-23 15:53:55 +0100

    Update strfetch.c

--- src/strfetch.c
+++ src/strfetch.c
@@ -10,32 +10,63 @@
 int main(int argc, char **argv) {
 	time_t randomiser;
 	uname(&kernel);
-    // strings
+
+	// color on/off
+	COLS = fopen(strcat(getenv("HOME"), "/.config/strfetch/conf"), "r");
+
+	if (!COLS) {
+	    color = 0;
+	} else {
+	    while(fscanf(COLS, "%s", word) != EOF) { 
+	        if(strcmp(word, "true") == 0) { 
+	            color = 1;
+	        } 
+	    } 
+ 	}
+ // strings
 	if (argc < 2) {
 		srand((unsigned) time(&randomiser));
-		printf("\x1b[31m[!] %s\n", strings[rand() % istrings]);
-	} else {
-		printf("\x1b[31m[!] ");
-		int i;
-		for( i=1; i<argc; i++ ) {
-        	printf("%s ", argv[i]);
+		if (color == 1) {
+		  printf("\x1b[31m");
 		}
+		printf("[!] %s\n", strings[rand() % istrings]);
+	 } else {
+		  printf("[!] ");
+		  int i = 1;
+		  for ( i < argc; i++; ) {
+      printf("%s ", argv[i]);
+		  }
 		printf("\n");
-	}
+	 }
 	// hostname
-	printf("\x1b[33m[&] Host: %s\n", kernel.nodename);
+	if (color == 1) {
+	    printf("\x1b[33m");
+	}
+	printf("[&] Host: %s\n", kernel.nodename);
 	// kernel version
-	printf("\x1b[32m[*] Kernel: %s %s %s\n", kernel.sysname, kernel.release, kernel.machine);
+	if (color == 1) {
+	  printf("\x1b[32m");
+	} 
+	printf("[*] Kernel: %s %s %s\n", kernel.sysname, kernel.release, kernel.machine);
 	// uptime
 	long uptime_seconds = 0;
+	if (color == 1) {
+   printf("\x1b[34m");
+	}
 	if (get_system_uptime(&uptime_seconds)) {
-        printf("\x1b[34m[*] Uptime: ");
-        format_uptime(uptime_seconds);
-    }
+   printf("[*] Uptime: ");
+   format_uptime(uptime_seconds);
+ }
 	// memory
-    printf("\x1b[35m[*] Memory: ");
-	get_memory_info();
-	// exit
-    printf("\x1b[0m"); // 1.1 broke people's shells.
+	if (color == 1) {
+	  printf("\x1b[35m");
+	}
+ printf("[*] Memory: ");
+ get_memory_info();
+ // exit
+ printf("\x1b[0m"); // 1.1 broke people's shells.
+	if (color == 1) {
+	    fclose(COLS);
+	}
 	return 0; 
 }
