e4283eaa59a7a2350d358f6eb242d75d873b7884
stx <80523829+stx3plus1@users.noreply.github.com>
2024-04-23 15:54:56 +0100

    Update func.c

--- src/func.c
+++ src/func.c
@@ -4,106 +4,62 @@
 // you may distribute and modify this code provided you give credit
 // i worked on this, 100%
 
-// headers we don't have due to separate files
+// version
+#define VERSION "1.1"
+
+// built in headers
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/utsname.h>
+#include <sys/types.h>
 
 #ifdef __linux__
-	#include <sys/stat.h>
-	#include <fcntl.h>
+    #define LINUX
 #elif defined(__APPLE__)
     #include <sys/sysctl.h>
-	#include <mach/mach.h>
+    #define MACOS
 #endif
 
-// uptime
-bool get_system_uptime(long *uptime_seconds) {
-#ifdef __linux__
-    FILE *file = fopen("/proc/uptime", "r");
-    if (file == NULL)
-        return false;
-    double uptime;
-    if (fscanf(file, "%lf", &uptime) != 1) {
-        fclose(file);
-        return false;
-    }
-    fclose(file);
-    *uptime_seconds = (long)uptime;
-    return true;
-#elif defined(__APPLE__)
-    int mib[2];
-    mib[0] = CTL_KERN;
-    mib[1] = KERN_BOOTTIME;
-    struct timeval boottime;
-    size_t length = sizeof(boottime);
-    if (sysctl(mib, 2, &boottime, &length, NULL, 0) < 0)
-        return false;
-    struct timeval now;
-    gettimeofday(&now, NULL);
-    *uptime_seconds = now.tv_sec - boottime.tv_sec;
-    return true;
-#else
-    return false;
-#endif
-}
+// strfetch info functions
+#include "func.h"
 
-// uptime printing
-void format_uptime(long uptime_seconds) {
-    int days = uptime_seconds / (60 * 60 * 24);
-    int hours = (uptime_seconds % (60 * 60 * 24)) / (60 * 60);
-    int minutes = (uptime_seconds % (60 * 60)) / 60;
-    if (days > 0) {
-		printf("%dd ", days);
-    }
-    if (hours > 0) {
-		printf("%dh ", hours);
-    }
-	printf("%dm\n", minutes);
-}
+// get user
+char homedir[64];
 
-// memory
-void get_memory_info() {    
-    #ifdef __APPLE__
-    int mib[2];
-    int64_t physical_memory;
-    size_t len = sizeof(physical_memory);
-    mib[0] = CTL_HW;
-    mib[1] = HW_MEMSIZE;
-    sysctl(mib, 2, &physical_memory, &len, NULL, 0);
-    mach_port_t host_port;
-    mach_msg_type_number_t host_size;
-    vm_size_t pagesize;
-    host_port = mach_host_self();
-    host_size = sizeof(vm_statistics_data_t) / sizeof(integer_t);
-    host_page_size(host_port, &pagesize);
-    vm_statistics_data_t vm_stat;
-    if (host_statistics(host_port, HOST_VM_INFO, (host_info_t)&vm_stat, &host_size) != KERN_SUCCESS) {
-		printf("Memory calculation error");
-        return;
-    }
-    natural_t mem_used = (vm_stat.active_count + vm_stat.inactive_count + vm_stat.wire_count) * pagesize;
-    int64_t free_memory = physical_memory - mem_used;
-    printf("%.2fGB / %.2fGB\n", (double)mem_used / (1024 * 1024) / 1024, (double)physical_memory / (1024 * 1024) / 1024);
-    #elif defined(__linux__)
-    FILE *file = fopen("/proc/meminfo", "r");
-    if (!file) {
-		printf("Memory calculation error");
-        return;
-    }
-    char line[256];
-    int total_memory = 0;
-    int free_memory = 0;
-    while (fgets(line, sizeof(line), file)) {
-        if (strncmp(line, "MemTotal:", 9) == 0) {
-            sscanf(line + 9, "%d", &total_memory);
-        } else if (strncmp(line, "MemFree:", 8) == 0) {
-            sscanf(line + 8, "%d", &free_memory);
-        }
-    }
-    fclose(file);
-    printf("%.2fGB / %.2fGB\n", (double)(total_memory - free_memory) / 1024 / 1024, (double)total_memory / 1024 / 1024);
-    #else
-    printf("Unsupported");
-    #endif
-}
+// color
+FILE* COLS;
+int color = 0;
+char word[32];
+
+// uname
+struct utsname kernel;
+
+// strings
+#define istrings 20
+// istrings = number of strings in the array
+char strings[istrings][128] = {
+	"i wonder what this button does",
+	"the world will end one day",
+	"time for your psu to blow up",
+	"del System32",
+	"systemd isnt bloatware",
+	"i will delete init rn",
+	"whats that behind you",
+	"mark zuck is the lizard man",
+	"your pc has a cpu",
+	"you shouldnt rm -rf /",
+	"i could like memleak rn",
+	"dude feed me",
+	"ubuntu makes pcs faster, canonical:",
+	"liblzma, xz 5.6.0 and 5.6.1",
+	"ssh wiimote.local",
+	"i (can) run in 200Î¼s",
+	"Segmentation Fault",
+	"is your pc case a cardboard box",
+	"pour water on me, im thirsty",
+	"make: *** No rule to make target `src/func.c', needed by `strfetch'. Stop."
+};
