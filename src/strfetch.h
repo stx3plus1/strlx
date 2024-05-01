3287209d65c5bea4c25dc2830857fa1ddc9de6ef
stx <80523829+stx3plus1@users.noreply.github.com>
2024-05-01 20:15:37 +0100

    Update strfetch.h

--- src/strfetch.h
+++ src/strfetch.h
@@ -17,7 +17,7 @@
 #include <sys/utsname.h>
 #include <sys/types.h>
 
-#ifdef __linux__
+#ifdef __linux__ || defined(__GNU__)
     #define LINUX
 #elif defined(__APPLE__)
     #include <sys/sysctl.h>
