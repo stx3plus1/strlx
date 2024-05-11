e8ae349c72e10152926ce7e07d94b21d471bcd1a
stx3plus1 <stx4@ballpa.in>
2024-05-11 12:05:06 +0100

    that's why it felt weird

--- src/ascii.h
+++ src/ascii.h
@@ -1,3 +1,9 @@
+// strfetch 
+// by cirkulx also known as stx3plus1 or stx4
+
+// you may distribute and modify this code provided you give credit
+// i worked on this, 100%
+
 char ascii_tux[][16] = {
 	"        ",
 	"  .-.   ",
